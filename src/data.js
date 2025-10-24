e6b2a30d17204b726be540533f3fa04fe537ce79
Kaung Khant Nay <kaungkhantnaynay@gmail.com>
2025-10-24 14:40:13 +0700

    Update Img

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
-export const profiles= [
-    {title:"Work", hours:32, last:36, color:"hsl(15, 100%, 70%)"},
-    {title:"Play", hours:10, last:8, color:"hsl(195, 74%, 62%)"},
-    {title:"Study", hours:4, last:7, color:"hsl(348, 100%, 68%)"},
-    {title:"Exercise", hours:4, last:5, color:"hsl(145, 58%, 55%)"},
-    {title:"Social", hours:5, last:10, color:"hsl(264, 64%, 52%)"},
-    {title:"Self-Care", hours:2, last:2, color:"hsl(43, 84%, 65%)"}
-]
+export const profiles = [
+  { title: "Work", hours: 32, last: 36, color: "hsl(15, 100%, 70%)", img: "/images/icon-work.svg" },
+  { title: "Play", hours: 10, last: 8, color: "hsl(195, 74%, 62%)", img: "/images/icon-play.svg" },
+  { title: "Study", hours: 4, last: 7, color: "hsl(348, 100%, 68%)", img: "/images/icon-study.svg" },
+  { title: "Exercise", hours: 4, last: 5, color: "hsl(145, 58%, 55%)", img: "/images/icon-exercise.svg" },
+  { title: "Social", hours: 5, last: 10, color: "hsl(264, 64%, 52%)", img: "/images/icon-social.svg"},
+  { title: "Self-Care", hours: 2, last: 2, color: "hsl(43, 84%, 65%)", img: "/images/icon-self-care.svg" },
+];
